3b3963312146ded9d3cf837c47264c41bf117093
Tạ Đặng Vĩnh Phúc <vinhphuctadang@gmail.com>
2020-02-07 15:25:19 +0700

    fixed: c script to run python

--- beaver.c
+++ beaver.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include <string.h> 
+#include <windows.h>
+
+char* getModuleFileDir () {
+	char* buffer = (char*) malloc (1025);
+	GetModuleFileName (NULL, buffer, 1025);
+	int idx = strlen (buffer)-1;
+	while (idx && buffer[idx] != '\\') buffer[idx] = '\0', --idx;
+	return buffer;
+}
+
 int __strlensum__ (int n, char* argv[]) {
 	int i,sum = 0;
 	for (i=0;i<n;++i) {
@@ -9,12 +19,16 @@ int __strlensum__ (int n, char* argv[]) {
 }
 
 int main(int argc, char *argv[] ) {
-	const char* exec = "python beaver.py";
+	const char* module = getModuleFileDir();
+	const char* exec = "python ";
+	const char* script = "beaver.py";
 	int len = __strlensum__(argc, argv), i;
-	char* str = (char*) malloc (strlen (exec) + len + 1);
+	char* str = (char*) malloc (strlen (module) + strlen (exec) + strlen (script) + len + 1);
 	memset (str, 0, sizeof (str));
 	
 	strcat (str, exec);
+	strcat (str, module);
+	strcat (str, script);
 	for (i=1; i<argc; ++i) {
 		strcat (str, " \"");
 		strcat (str, argv[i]);
